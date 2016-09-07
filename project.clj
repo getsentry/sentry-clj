175f96f00172b604c6f6fe3dcbd4b5e488b75423
Coda Hale <coda.hale@gmail.com>
2016-09-06 18:25:37 -0700

    Fix scope of ring-core dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[cheshire "5.6.3"]
                  [clj-time "0.12.0"]
                  [com.getsentry.raven/raven "7.7.0"]
-                 [ring/ring-core "1.5.0" :scope "optional"]]
+                 [ring/ring-core "1.5.0" :scope "provided"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :aot [raven-clj.internal]
