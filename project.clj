9ee2e219af69e30e52f04e1c8a5187d389043585
Coda Hale <coda.hale@gmail.com>
2017-03-28 11:43:35 -0700

    Upgrade Raven and slf4j-jcl.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.7.0"]
                  [clj-time "0.13.0"]
-                 [com.getsentry.raven/raven "7.8.3"]
+                 [com.getsentry.raven/raven "7.8.6"]
                  [ring/ring-core "1.5.1" :scope "provided"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
@@ -13,7 +13,7 @@
              :test [:project/test :profiles/test]
              :profiles/dev {:dependencies [[mocko "0.2.3"]
                                            [org.clojure/clojure "1.8.0"]
-                                           [org.slf4j/slf4j-jcl "1.7.24"]]}
+                                           [org.slf4j/slf4j-jcl "1.7.25"]]}
              :profiles/test {}
              :project/dev {:source-paths ["dev"]
                            :repl-options {:init-ns user}}
