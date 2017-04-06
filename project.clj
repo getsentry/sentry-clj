3ac6479003b97f326e4835d349454be6cd30a560
Coda Hale <coda.hale@gmail.com>
2017-04-05 19:02:19 -0700

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codahale/raven-clj "0.2.0"
+(defproject com.codahale/raven-clj "0.2.1-SNAPSHOT"
   :description "A Clojure client for Sentry."
   :url "https://github.com/codahale/raven-clj"
   :license {:name "Eclipse Public License"
