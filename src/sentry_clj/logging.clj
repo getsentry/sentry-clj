f15a7ce933dbb7c4c052fc49303ba8344f9a2718
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-09-12 01:58:00 +0100

    Add all main logging function

--- src/sentry_clj/logging.clj
+++ src/sentry_clj/logging.clj
@@ -1,9 +1,24 @@
 (ns sentry-clj.logging
+  "Structured logging integration with Sentry.
+   
+   Provides logging functions at all standard levels:
+   trace, debug, info, warn, error, fatal"
   (:import [io.sentry Sentry]))
 
-; TODO: improve with other log levels and generic log function
-(defn log!
-  [message & args]
-  (let [array-params (when (seq args)
-                       (into-array Object args))]
-    (.info (Sentry/logger) message array-params)))
+(defmacro deflogger
+  "Defines a logging function for the given level."
+  [level]
+  `(defn ~level
+     ~(str "Log a message at " level " level with optional format arguments.")
+     [message# & args#]
+     (let [array-params# (when (seq args#)
+                          (into-array Object args#))]
+       (~(symbol (str "." level)) (Sentry/logger) message# array-params#))))
+
+; Generate all logging functions
+(deflogger trace)
+(deflogger debug) 
+(deflogger info)
+(deflogger warn)
+(deflogger error)
+(deflogger fatal)
